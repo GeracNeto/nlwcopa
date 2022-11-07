c503919238f8859206a7fbac81d75955ee6d53a0
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-11-07 19:36:01 -0300

    Criadas as rotas para criação de bolão, contagem de bolões, contagem de usuários e contagem de palpites

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -20,13 +22,55 @@ async function bootstrap() {
         origin: true
     })
 
-    // http://localhost:3333/pools/count
+    // Mostra o número de bolões
     fastify.get('/pools/count', async () => {
 
         // conta quantos dados existem na tabela 'pool' -> númeor de bolões criados
         const count = await prisma.pool.count()
 
         return { count }
+    })
+
+    // Conta os usuários
+    fastify.get('/users/count', async () => {
+
+        // conta quantos dados existem na tabela 'pool' -> númeor de bolões criados
+        const count = await prisma.user.count()
+
+        return { count }
+    })
+
+    //
+    fastify.get('/guesses/count', async () => {
+
+        // conta quantos dados existem na tabela 'pool' -> númeor de bolões criados
+        const count = await prisma.guess.count()
+
+        return { count }
+    })
+
+    // Criar bolão
+    fastify.post('/pools', async (request, reply) => {
+
+        // Uso da lib zod
+        const createPoolBody = z.object({
+            title: z.string()
+        })
+
+        const { title } = createPoolBody.parse(request.body)
+
+        // Gera um código de 6 caracteres
+        const generate = new ShortUniqueId({ length: 6 })
+        const code = String(generate()).toUpperCase() // Deixa o código em caixa alta
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })
+
+        return reply.status(201).send({ code })
     })
 
     // Servidor rodando na porta 3333, o host: 0.0.0.0 é para o mobile poder consumir
